e2351eb85b7ce8b700add521d8776ad7996f7668
malika099 <ergasevamadina77@gmail.com>
2024-12-07 11:23:21 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+       /* int[] array = {2,8,3,3,4,4,9,19};
+        System.out.print("Минимальный элемент: ");
+        System.out.println(min(array));
+        System.out.print("Максимальный элемент: ");
+        System.out.println(max(array));
+    }
+    public static int min(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+
+         }
+    public static int max(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;*/
+        
+    }
+
+    }
+
+
+
